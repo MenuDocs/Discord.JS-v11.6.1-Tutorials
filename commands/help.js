aa85a9f9dbeab87f34df29cc35b448d46ac3dcf2
Connor Strand <42255832+Strandxo@users.noreply.github.com>
2019-01-15 12:28:48 +1030

    Add files via upload

---
+++ commands/help.js
@@ -0,0 +1,50 @@
+const Discord = require("discord.js")
+const botconfig = require("../botconfig.json");
+const colours = require("../colours.json");
+const prefix = botconfig.prefix
+
+
+module.exports.run = async (bot, message, args) => {
+
+    if(args[0] == "help") return message.channel.send(`Just do ${prefix}help instead.`)
+
+    if(args[0]) {
+        let command = args[0];
+        if(bot.commands.has(command)) {
+            command = bot.commands.get(command);
+            var SHembed = new Discord.RichEmbed()
+            .setColor(colours.cyan)
+            .setAuthor(`TestBOT HELP`, message.guild.iconURL)
+            .setThumbnail(bot.user.displayAvatarURL)
+            .setDescription(`The bot prefix is: ${prefix}\n\n**Command:** ${command.config.name}\n**Description:** ${command.config.description || "No Description"}\n**Usage:** ${command.config.usage || "No Usage"}\n**Accessable by:** ${command.config.accessableby || "Members"}\n**Aliases:** ${command.config.noalias || command.config.aliases}`)
+            message.channel.send(SHembed);
+        }}
+
+    if(!args[0]) {
+        message.delete();
+        let embed = new Discord.RichEmbed()
+        .setAuthor(`Help Command!`, message.guild.iconURL)
+        .setColor(colours.redlight)
+        .setDescription(`${message.author.username} check your dms!`)
+
+        let Sembed = new Discord.RichEmbed()
+        .setColor(colours.cyan)
+        .setAuthor(`TestBOT Help`, message.guild.iconURL)
+        .setThumbnail(bot.user.displayAvatarURL)
+        .setTimestamp()
+        .setDescription(`These are the avaliable commands for the TestBOT!\nThe bot prefix is: ${prefix}`)
+        .addField(`Commands:`, "``cat`` ``dog`` ``meme`` ``help`` ``serverinfo`` ``userinfo``")
+        .setFooter("Test Bot 2k18", bot.user.displayAvatarURL)
+        message.channel.send(embed).then(m => m.delete(10000));
+        message.author.send(Sembed)
+    }
+}
+
+
+module.exports.config = {
+    name: "help",
+    aliases: ["h", "halp", "commands"],
+    usage: "!usage",
+    description: "",
+    accessableby: "Members"
+}

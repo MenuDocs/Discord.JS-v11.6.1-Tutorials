cbc6b3cb79f79c3ecf750a0d5b8e2d840ddefcc2
Connor Strand <42255832+Strandxo@users.noreply.github.com>
2019-03-20 22:40:51 +1030

    Add files via upload

---
+++ commands/say.js
@@ -0,0 +1,28 @@
+const Discord = require("discord.js")
+
+module.exports.run = async (bot, message, args) => {
+
+    if(!message.member.hasPermission(["MANAGE_MESSAGES", "ADMINISTRATOR"])) return message.channel.send("You can not use this command!")
+    
+    let argsresult;
+    let mChannel = message.mentions.channels.first()
+
+    message.delete()
+    if(mChannel) {
+        argsresult = args.slice(1).join(" ")
+        mChannel.send(argsresult)
+    } else {
+        argsresult = args.join(" ")
+        message.channel.send(argsresult)
+    }
+
+}
+
+
+module.exports.config = {
+    name: "say",
+    description: "sends a message that was inputted to a channel",
+    usage: "!say",
+    accessableby: "Staff",
+    aliases: ["acc", "announcement"]
+}

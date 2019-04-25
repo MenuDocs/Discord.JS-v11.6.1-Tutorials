299f04e92bd033a6f39309116a83813e005bcfc8
Connor Strand <42255832+Strandxo@users.noreply.github.com>
2019-04-25 18:03:59 +0930

    Delete disconnect.js

--- events/client/disconnect.js
+++
@@ -1,5 +0,0 @@
-const dateformat = require("dateformat");
-
-module.exports = () => {
-    console.log(`you have been disconnected at ${dateformat(new Date(), "dddd, mmmm dS, yyyy, h:MM:ss TT")}.`)
-}

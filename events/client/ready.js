98cf0e34e7c6a0cda37825ae795961ec62a6e704
Connor Strand <42255832+Strandxo@users.noreply.github.com>
2019-12-19 00:25:30 +1030

    finna get banned

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,13 +1,23 @@
-const { PlayerManager } = require("discord.js-lavalink");
+const { ErelaClient, Utils } = require("erela.js");
 const { nodes } = require("../../botconfig.json")
 
 module.exports = bot => {
     console.log(`${bot.user.username} is online`);
 
-    // global.lavalink = new PlayerManager(bot, nodes, {
-    //     user: bot.user.id,
-    //     shards: 0
-    // });
+    bot.music = new ErelaClient(bot, nodes)
+        .on("nodeError", console.log)
+        .on("nodeConnect", () => console.log("Successfully created a new Node."))
+        .on("queueEnd", player => {
+            player.textChannel.send("Queue has ended.")
+            return bot.music.players.destroy(player.guild.id)
+        })
+        .on("trackStart", ({textChannel}, {title, duration}) => textChannel.send(`Now playing: **${title}** \`${Utils.formatTime(duration, true)}\``).then(m => m.delete(15000)));
+
+    bot.levels = new Map()
+        .set("none", 0.0)
+        .set("low", 0.10)
+        .set("medium", 0.15)
+        .set("high", 0.25);
 
     let activities = [ `${bot.guilds.size} servers!`, `${bot.channels.size} channels!`, `${bot.users.size} users!` ], i = 0;
     setInterval(() => bot.user.setActivity(`${bot.prefix}help | ${activities[i++ % activities.length]}`, { type: "WATCHING" }), 15000)

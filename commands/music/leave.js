2a715cefd068c2a001d2b95d925b529640e97a3a
Connor Moriarty <42255832+Strandxo@users.noreply.github.com>
2020-01-11 17:19:03 +1030

    Adding Leave command code.

---
+++ commands/music/leave.js
@@ -0,0 +1,19 @@
+module.exports = { 
+    config: {
+        name: "leave",
+        aliases: ["lev", "stop"],
+        description: "Makes the bot leave the voice channel.",
+        accessableby: "Member",
+        category: "music",
+    },
+    run: async (bot, message, args) => {
+        const { voiceChannel } = message.member;
+        const player = bot.music.players.get(message.guild.id);
+
+        if(voiceChannel && voiceChannel.id !== player.voiceChannel.id) return message.channel.send("You need to be in a voice channel to use the leave command.");
+        if(!player) return message.channel.send("No song/s currently playing in this guild.");
+
+        bot.music.players.destroy(message.guild.id);
+        return message.channel.send("Successfully stopped the music.")
+    }
+}

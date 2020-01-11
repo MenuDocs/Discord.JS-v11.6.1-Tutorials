481dd917fd6cea367f4c69895f11bbedec7b4bbe
Connor Moriarty <42255832+Strandxo@users.noreply.github.com>
2020-01-11 17:13:42 +1030

    Fixing overlooked mistake.

--- commands/miscellaneous/urban.js
+++ commands/miscellaneous/urban.js
@@ -14,7 +14,7 @@ module.exports = {
     },
     run: async (bot, message, args) => {
         if(!message.channel.nsfw) return message.channel.send("Please run this command in a `NSFW` channel.");
-        if(args < 1 || !["search", "random"].includes(args[0])) return message.channel.send("`-urban <search|random> (query)`");
+        if(!args[0] || !["search", "random"].includes(args[0])) return message.channel.send("`-urban <search|random> (query)`");
         let image = "http://cdn.marketplaceimages.windowsphone.com/v8/images/5c942bfe-6c90-45b0-8cd7-1f2129c6e319?imageType=ws_icon_medium";
         let search = args[1] ? urban(args.slice(1).join(" ")) : urban.random();
             try {

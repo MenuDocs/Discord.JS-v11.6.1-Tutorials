754fc249cc30b96d4d5681d18479f7b73de853f1
Connor Moriarty <42255832+Strandxo@users.noreply.github.com>
2020-01-23 10:18:41 +1030

    Fixing error missed by myself & reviewers.

--- commands/music/leave.js
+++ commands/music/leave.js
@@ -10,8 +10,8 @@ module.exports = {
         const { voiceChannel } = message.member;
         const player = bot.music.players.get(message.guild.id);
 
-        if(voiceChannel && voiceChannel.id !== player.voiceChannel.id) return message.channel.send("You need to be in a voice channel to use the leave command.");
         if(!player) return message.channel.send("No song/s currently playing in this guild.");
+        if(!voiceChannel || voiceChannel.id !== player.voiceChannel.id) return message.channel.send("You need to be in a voice channel to use the leave command.");
 
         bot.music.players.destroy(message.guild.id);
         return message.channel.send("Successfully stopped the music.")
